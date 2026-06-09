234168982d8aae6fc6328c67a956c23f9a1d2f3f
felixwaryoba-jpg <felixwaryoba@gmail.com>
2026-06-09 11:24:57 -0700

    Custom text field widget

---
+++ lib/widgets/app_text_field.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class AppTextField extends StatelessWidget {
+  final String label;
+  final String? hint;
+  final TextEditingController? controller;
+  final TextInputType keyboardType;
+  final bool obscureText;
+  final TextInputAction textInputAction;
+  final Function(String)? onChanged;
+  final String? Function(String?)? validator;
+  final int? maxLines;
+  final int? minLines;
+  final Widget? suffixIcon;
+  final Widget? prefixIcon;
+
+  const AppTextField({
+    Key? key,
+    required this.label,
+    this.hint,
+    this.controller,
+    this.keyboardType = TextInputType.text,
+    this.obscureText = false,
+    this.textInputAction = TextInputAction.next,
+    this.onChanged,
+    this.validator,
+    this.maxLines = 1,
+    this.minLines,
+    this.suffixIcon,
+    this.prefixIcon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                fontWeight: FontWeight.w600,
+              ),
+        ),
+        const SizedBox(height: 8),
+        TextFormField(
+          controller: controller,
+          keyboardType: keyboardType,
+          obscureText: obscureText,
+          textInputAction: textInputAction,
+          onChanged: onChanged,
+          validator: validator,
+          maxLines: obscureText ? 1 : maxLines,
+          minLines: minLines,
+          decoration: InputDecoration(
+            hintText: hint,
+            suffixIcon: suffixIcon,
+            prefixIcon: prefixIcon,
+          ),
+        ),
+      ],
+    );
+  }
+}
